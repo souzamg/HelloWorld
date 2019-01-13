a1165a96ceebf1588c71c21eb4426ff6f0919bcd
souzamg <SOUZAMG@na.ad.whirlpool.com>
2019-01-12 19:39:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/souzamg/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.souzamg.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
+        Log.d("MainActivity","Hello World");
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countToast(View view) {
+        mCount++;
+        mShowCount.setText(Integer.toString((mCount)));
+    }
+}
